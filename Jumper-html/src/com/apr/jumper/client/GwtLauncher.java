7d46d3caf51011a5c526d785cf71c72e1c486446
Alejandro Pasos <apr_159@yahoo.com>
2014-04-10 13:14:01 -0500

    First version
    Initial project files

---
+++ Jumper-html/src/com/apr/jumper/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.apr.jumper.client;
+
+import com.apr.jumper.JumperGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new JumperGame();
+	}
+}
